68d326f86fc5d47416de0278c48e3f90564105ac
Subhojeet Mukhopadhyay <msubhojeet@gmail.com>
2022-06-28 21:28:32 +0530

    Added react router adn made pages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+      <Route>404 error page not found!!!</Route>
+    </Switch>
+  );
+}
+
+export default App;
