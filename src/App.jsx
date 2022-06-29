82f779ad1984623ef8ba230672c2b28d4abbb2fe
Subhojeet Mukhopadhyay <msubhojeet@gmail.com>
2022-06-29 17:38:50 +0530

    added router linking to starred and home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
-      <Route exact path="/starred">
-        This is starred
-      </Route>
-      <Route>404 error page not found!!!</Route>
-    </Switch>
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Starred = () => {
+  return <div>This is Starred</div>;
+};
+
+export default Starred;
