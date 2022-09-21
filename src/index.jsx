31dbccf3d0076db0f9e4b7fc53d236d22f747c82
Raider-9 <100202068+Raider-9@users.noreply.github.com>
2022-09-21 20:07:18 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
